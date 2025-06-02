0fb179e35e4725161a52c5ac6812b648c1c863e1
Ramanjaneyulu-2003 <Ramanjaneyulu-2003>
2025-06-02 19:59:25 +0530

    maybatch

---
+++ core/src/main/java/com/adobe/aem/may/batch/core/services/EmpConfiguration.java
@@ -0,0 +1,27 @@
+package com.adobe.aem.may.batch.core.services;
+
+import org.osgi.service.metatype.annotations.AttributeDefinition;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+
+@ObjectClassDefinition(name = "EmpConfiguration")
+public @interface EmpConfiguration {
+    @AttributeDefinition(
+        name = "raju",
+        description ="Raju isa good boy",
+        required = true
+    )
+    public String empName();
+    @AttributeDefinition(
+        name = "300",
+        description ="this is lucky number",
+        required = true
+    )
+    public int empId();
+    @AttributeDefinition(
+        name = "200000",
+        description ="good sal",
+        required = true
+    )
+    public double empSal();
+
+}
